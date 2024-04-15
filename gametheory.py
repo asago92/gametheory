10417ed27df09c2be77752c42ba45bfb276c35bf
asago92 <101930040+asago92@users.noreply.github.com>
2024-04-15 10:16:56 +0200

    Update gametheory.py

--- gametheory.py
+++ gametheory.py
@@ -2,26 +2,18 @@ import streamlit as st
 import gambit
 
 def create_game():
-    game = gambit.Game.new_table([2, 2])
-
-    # Define players and actions
+    # Define the players and actions
     players = ["Player 1 (Wakes Up)", "Player 2 (Doesn't Wake Up)"]
     actions = ["Wakes Up", "Doesn't Wake Up"]
-    game.players = players
-    game.actions = actions
+
+    # Create the game
+    game = gambit.NormalFormGame(players, actions)
 
     # Define payoffs
-    game[0, 0][0] = 1  # Player 1 wakes up, Player 2 wakes up
-    game[0, 0][1] = 1
-
-    game[0, 1][0] = 0  # Player 1 wakes up, Player 2 doesn't wake up
-    game[0, 1][1] = 2
-
-    game[1, 0][0] = 2  # Player 1 doesn't wake up, Player 2 wakes up
-    game[1, 0][1] = 0
-
-    game[1, 1][0] = 0  # Player 1 doesn't wake up, Player 2 doesn't wake up
-    game[1, 1][1] = 0
+    game[0, 0] = (1, 1)  # Player 1 wakes up, Player 2 wakes up
+    game[0, 1] = (0, 2)  # Player 1 wakes up, Player 2 doesn't wake up
+    game[1, 0] = (2, 0)  # Player 1 doesn't wake up, Player 2 wakes up
+    game[1, 1] = (0, 0)  # Player 1 doesn't wake up, Player 2 doesn't wake up
 
     return game
 
@@ -35,3 +27,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
