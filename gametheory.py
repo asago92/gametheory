3fff43fd78286ee5e2aed4bf0123f245277eff77
asago92 <101930040+asago92@users.noreply.github.com>
2024-04-15 10:38:46 +0200

    Update gametheory.py

--- gametheory.py
+++ gametheory.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import nashpy as nash
 import numpy as np
+import matplotlib.pyplot as plt
 
 def create_game():
     # Define the payoff matrices for the players
@@ -13,18 +14,34 @@ def create_game():
     game = nash.Game(A, B)
     return game
 
+def plot_payoff_matrix(game):
+    fig, ax = plt.subplots()
+    matrix = np.dstack(game.payoff_matrices)
+    cax = ax.matshow(matrix, cmap='coolwarm')
+
+    # Set up axes
+    ax.set_xticklabels([''] + ["Wakes Up", "Doesn't Wake Up"], rotation=45)
+    ax.set_yticklabels([''] + ["Player 1", "Player 2"])
+
+    # Loop over data dimensions and create text annotations.
+    for i in range(matrix.shape[0]):
+        for j in range(matrix.shape[1]):
+            ax.text(j, i, f'{game.payoff_matrices[0][i, j]}\n{game.payoff_matrices[1][i, j]}', ha='center', va='center', color='black')
+
+    plt.colorbar(cax, ax=ax)
+    return fig
+
 def main():
     st.title("Payoff Matrix for Waking Up Early")
 
     if st.button("Show Payoff Matrix"):
         game = create_game()
-        st.write("Payoff Matrix for Player 1 (Row player):")
-        st.write(game.payoff_matrices[0])
-        st.write("Payoff Matrix for Player 2 (Column player):")
-        st.write(game.payoff_matrices[1])
+        fig = plot_payoff_matrix(game)
+        st.pyplot(fig)
 
 if __name__ == "__main__":
     main()
+
 
 
 
